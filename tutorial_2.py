55c61bc208f66987ce80113af01cdfb8a6a0cedd
Rupali Shinde <31463957+RupsK@users.noreply.github.com>
2024-09-23 17:34:57 +0900

    class 2nd
    This is python code for comment line and mathematical calculation

---
+++ tutorial_2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 11:17:03 2024
+
+@author: h4tec
+"""
+# this is fpr comment 
+text = "# this is not comment becuase inside the bracket"
+
+test = "this is only string" # this is comment
+
+# Now lets start the real calculator
+
+a  = 5
+b = 5
+
+c = a +b # this is addition
+print(c)
+
+d = a -b    # this is substraction
+print(d) 
+
+e = a * b   # this is multiplication
+print(e)
+
+f = a^2 # this is 
+print("f="+ str(f))
+
+g = 17 / 3 #
+
+print('f'+str(g))
+
+h = 17// 3
+
+print(h)
+
+i = a % b
+print(i)
