da209facb252cd728d572afc270300f2b75b99ab
Rupali Shinde <31463957+RupsK@users.noreply.github.com>
2024-09-24 11:42:59 +0900

    Add files via upload

---
+++ tutorial_3.py
@@ -0,0 +1,18 @@
+
+
+word = 'Python'
+
+sentence = "This is interpreter based langguage"
+
+para = """This is lomg statement in Python. It will be usefull for web design. I think evryone must learn Python!!!"""
+
+print(para)
+
+# now check deleting the Python veriable
+
+count = 100
+print(count)
+
+del count  # to delete the verible
+
+print(count)
