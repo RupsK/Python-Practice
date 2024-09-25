15a434cabd253d89f961dc3c81e31df683a00ce4
Rupali Shinde <31463957+RupsK@users.noreply.github.com>
2024-09-25 14:43:36 +0900

    Variable access

---
+++ tutorial4_variableOutput.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 14:32:52 2024
+
+@author: h4tec
+"""
+
+x =  "Python is awesome"
+print("** it is full string**")
+print(x)
+
+x = "python"
+y = "3.x"
+z =  "awesome"
+print("** it is print string with + operator**")
+print( x + y+ z)
+
+print("** it is print string with , comma operator**")
+print( x , y, z)
